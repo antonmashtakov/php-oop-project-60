41b9c6544553a2da3fe852fb59e382065ef6fb3b
Антон Маштаков <antonmashtakov@MacBook-Pro-Anton.local>
2025-06-30 16:58:30 +0300

    Убран лишний код

--- src/Validator.php
+++ src/Validator.php
@@ -10,20 +10,4 @@ class Validator
     {
         return new StringValidator();
     }
-//    public function __construct($level = 'easy')
-//    {
-//        switch ($level) {
-//            case 'easy':
-//                $this->strategy = new strategies\Easy();
-//                break;
-//            case 'normal':
-//                $this->strategy = new strategies\Normal();
-//                break;
-//        }
-//        $this->map = [
-//            1 => array_fill(1, 3, null),
-//            2 => array_fill(1, 3, null),
-//            3 => array_fill(1, 3, null)
-//        ];
-//    }
 }
